c89d02faddfc799a3b04f1708dfe6bb45030798b
iZ17 <zarradiheb007@gmail.com>
2024-11-28 09:35:32 +0100

    ready to upload

--- middlewares/restrictTime.js
+++ middlewares/restrictTime.js
@@ -4,7 +4,7 @@ const day = new Date().getDay();
 const time = new Date().getHours();
 
 router.get("", (req, res, next) => {
-  if (day >= 1 && day <= 5 && time >= 9 && time <= 22) next();
+  if (day >= 1 && day <= 5 && time >= 9 && time <= 17) next();
   else res.render("access-denied");
 });
 
